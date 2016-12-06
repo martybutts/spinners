f55ba9b5f46667c88050de77a53d34389bcb8ef2
Martin <martinbutts@clear.net.nz>
2016-12-06 13:19:35 +1300

    third spinner added without fade

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var app = express()
 app.use(express.static(__dirname + '/public'))
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Spinners app listening on port 3000!')
 })
